f39aadd7683fb38522bf4cc52265613f853963d3
Niklas <niklas.dominik.krieg@hs-furtwangen.de>
2020-06-06 00:09:56 +0200

    Daten und Skript getrennt in 2 Skripte

--- Aufgaben/Aufgabe05/script.js
+++ Aufgaben/Aufgabe05/script.js
@@ -1,125 +1,34 @@
 "use strict";
-var Aufgabe06;
-(function (Aufgabe06) {
-    //#region articles Array
-    let articles = [
-        {
-            image: "../Aufgabe04/Artikelbilder/kletterschuhe-beginner.jpg",
-            name: "La Sportiva Tarantula",
-            desc: "Ideal als erster Kletterschuh, endlich keine Leihschuhe mehr!",
-            price: 79.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/kletterschuhe-intermediate.jpg",
-            name: "Scarpa Arpia",
-            desc: "Bequemer Einstieg in die Welt der High Performance Schuhe.",
-            price: 119.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/instinct-kletterschuhe.jpg",
-            name: "Scarpa Instinct VS-R",
-            desc: "Weichere Version des bewährten Instinct VS in schickem Blau.",
-            price: 144.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/kletterschuhe-pro.jpg",
-            name: "La Sportiva Skwama",
-            desc: "La Sportivas erfolgreichster High Performance Schuh.",
-            price: 149.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/kletterschuhe-pro2.jpg",
-            name: "Scarpa Drago",
-            desc: "Idealer Schuh für schwere, überhängende Boulder.",
-            price: 149.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/furia-kletterschuhe.jpg",
-            name: "Scarpa Furia Air",
-            desc: "Starke Vorspannung, hohe Asymmetrie und geringes Gewicht",
-            price: 159.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/boulderbucket-moon.jpg",
-            name: "Chalkbag",
-            desc: "Geräumiger Chalkbucket von Moon in schlichtem Schwarz.",
-            price: 29.99,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/block-chalk.jpg",
-            name: "Chalk Block",
-            desc: "Magnesiumcarbonat in Blockform zum selbst zerkleinern.",
-            price: 2.49,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/boulderbuerste.jpg",
-            name: "Boulderbürste",
-            desc: "Zur mühelosen und gründlichen Reinigung mittlerer und großer Griffe.",
-            price: 5.99,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/climb-on.jpg",
-            name: "Climb On Hautpflege",
-            desc: "Hilft bei durchs Klettern strapazierter Haut schnell und effektiv.",
-            price: 8.49,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/tape.jpg",
-            name: "Klettertape",
-            desc: "Sehr hilfreich bei Wunden oder zum Stabilisieren der Gelenke.",
-            price: 4.99,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/crashpad.jpg",
-            name: "Crashpad",
-            desc: "Mittelgroßes Crashpad für regelmäßige Outdoorseesions.",
-            price: 219.99,
-            category: "gear"
-        }
-    ];
-    //#endregion 
+var Aufgabe05;
+(function (Aufgabe05) {
     //formatiert den price von Typ number in die gängige Euro Preis Darstellung um
-    function formatNumberToEuro(_price) {
-        return _price.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    }
     //#region for schleife
-    for (let i = 0; i < articles.length; i++) {
+    for (let i = 0; i < Aufgabe05.articles.length; i++) {
         let articleDiv = document.createElement("div");
         articleDiv.classList.add("article");
         let articleName = document.createElement("h2");
-        articleName.innerText = articles[i].name;
+        articleName.innerText = Aufgabe05.articles[i].name;
         articleName.classList.add("article-name");
         articleDiv.appendChild(articleName);
         let articleIMG = document.createElement("img");
-        articleIMG.setAttribute("alt", articles[i].name);
-        articleIMG.setAttribute("src", articles[i].image);
+        articleIMG.setAttribute("alt", Aufgabe05.articles[i].name);
+        articleIMG.setAttribute("src", Aufgabe05.articles[i].image);
         articleIMG.classList.add("article-img");
         articleDiv.appendChild(articleIMG);
         let articleDesc = document.createElement("p");
-        articleDesc.innerText = articles[i].desc;
+        articleDesc.innerText = Aufgabe05.articles[i].desc;
         articleDesc.classList.add("article-desc");
         articleDiv.appendChild(articleDesc);
         let articlePrice = document.createElement("p");
-        articlePrice.innerText = formatNumberToEuro(articles[i].price);
+        articlePrice.innerText = Aufgabe05.articles[i].price.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
         articlePrice.classList.add("article-price");
         articleDiv.appendChild(articlePrice);
         let articleButton = document.createElement("button");
         articleButton.innerText = "In den Einkaufswagen";
         articleButton.classList.add("article-button");
         articleDiv.appendChild(articleButton);
-        document.getElementById(articles[i].category + "-cat")?.appendChild(articleDiv);
+        document.getElementById(Aufgabe05.articles[i].category + "-cat")?.appendChild(articleDiv);
     }
     //#endregion
-})(Aufgabe06 || (Aufgabe06 = {}));
+})(Aufgabe05 || (Aufgabe05 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgaben/Aufgabe05/script.ts
+++ Aufgaben/Aufgabe05/script.ts
@@ -1,107 +1,9 @@
-namespace Aufgabe06 {
+namespace Aufgabe05 {
 
-    interface ArtikelBouldern {
-        image: string;
-        name: string;
-        desc: string;
-        price: number;
-        category: string;
-    }
-//#region articles Array
-    let articles: ArtikelBouldern[] = [
-        {
-            image: "../Aufgabe04/Artikelbilder/kletterschuhe-beginner.jpg",
-            name: "La Sportiva Tarantula",
-            desc: "Ideal als erster Kletterschuh, endlich keine Leihschuhe mehr!",
-            price: 79.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/kletterschuhe-intermediate.jpg",
-            name: "Scarpa Arpia",
-            desc: "Bequemer Einstieg in die Welt der High Performance Schuhe.",
-            price: 119.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/instinct-kletterschuhe.jpg",
-            name: "Scarpa Instinct VS-R",
-            desc: "Weichere Version des bewährten Instinct VS in schickem Blau.",
-            price: 144.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/kletterschuhe-pro.jpg",
-            name: "La Sportiva Skwama",
-            desc: "La Sportivas erfolgreichster High Performance Schuh.",
-            price: 149.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/kletterschuhe-pro2.jpg",
-            name: "Scarpa Drago",
-            desc: "Idealer Schuh für schwere, überhängende Boulder.",
-            price: 149.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/furia-kletterschuhe.jpg",
-            name: "Scarpa Furia Air",
-            desc: "Starke Vorspannung, hohe Asymmetrie und geringes Gewicht",
-            price: 159.99,
-            category: "shoes"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/boulderbucket-moon.jpg",
-            name: "Chalkbag",
-            desc: "Geräumiger Chalkbucket von Moon in schlichtem Schwarz.",
-            price: 29.99,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/block-chalk.jpg",
-            name: "Chalk Block",
-            desc: "Magnesiumcarbonat in Blockform zum selbst zerkleinern.",
-            price: 2.49,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/boulderbuerste.jpg",
-            name: "Boulderbürste",
-            desc: "Zur mühelosen und gründlichen Reinigung mittlerer und großer Griffe.",
-            price: 5.99,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/climb-on.jpg",
-            name: "Climb On Hautpflege",
-            desc: "Hilft bei durchs Klettern strapazierter Haut schnell und effektiv.",
-            price: 8.49,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/tape.jpg",
-            name: "Klettertape",
-            desc: "Sehr hilfreich bei Wunden oder zum Stabilisieren der Gelenke.",
-            price: 4.99,
-            category: "gear"
-        },
-        {
-            image: "../Aufgabe04/Artikelbilder/crashpad.jpg",
-            name: "Crashpad",
-            desc: "Mittelgroßes Crashpad für regelmäßige Outdoorseesions.",
-            price: 219.99,
-            category: "gear"
-        }
-    ];
-
-    //#endregion 
 
     //formatiert den price von Typ number in die gängige Euro Preis Darstellung um
 
-    function formatNumberToEuro(_price: number): string {
-        return _price.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
-    }
+   
 
     //#region for schleife
 
@@ -127,7 +29,7 @@ namespace Aufgabe06 {
         articleDiv.appendChild(articleDesc);
 
         let articlePrice: HTMLParagraphElement = document.createElement("p");
-        articlePrice.innerText = formatNumberToEuro(articles[i].price);
+        articlePrice.innerText = articles[i].price.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
         articlePrice.classList.add("article-price");
         articleDiv.appendChild(articlePrice);
 
