417b2f79a359769158c1cde44b4eca6003a30b7c
Niklas <niklas.dominik.krieg@hs-furtwangen.de>
2020-06-09 19:44:35 +0200

    Erstellen Aufgabe07

---
+++ Aufgaben/Aufgabe07/script.js
@@ -0,0 +1,76 @@
+"use strict";
+var Aufgabe07;
+(function (Aufgabe07) {
+    for (let i = 0; i < Aufgabe07.articles.length; i++) {
+        let articleDiv = document.createElement("div");
+        articleDiv.classList.add("article");
+        let articleName = document.createElement("h2");
+        articleName.innerText = Aufgabe07.articles[i].name;
+        articleName.classList.add("article-name");
+        articleDiv.appendChild(articleName);
+        let articleIMG = document.createElement("img");
+        articleIMG.setAttribute("alt", Aufgabe07.articles[i].name);
+        articleIMG.setAttribute("src", Aufgabe07.articles[i].image);
+        articleIMG.classList.add("article-img");
+        articleDiv.appendChild(articleIMG);
+        let articleDesc = document.createElement("p");
+        articleDesc.innerText = Aufgabe07.articles[i].desc;
+        articleDesc.classList.add("article-desc");
+        articleDiv.appendChild(articleDesc);
+        let articlePrice = document.createElement("p");
+        articlePrice.innerText = Aufgabe07.articles[i].price.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
+        articlePrice.classList.add("article-price");
+        articleDiv.appendChild(articlePrice);
+        let articleButton = document.createElement("button");
+        articleButton.innerText = "In den Einkaufswagen";
+        articleButton.classList.add("article-button");
+        articleButton.setAttribute("article_price", Aufgabe07.articles[i].price.toString());
+        articleButton.addEventListener("click", handleAddToCartClick);
+        articleDiv.appendChild(articleButton);
+        document.getElementById(Aufgabe07.articles[i].category + "-cat")?.appendChild(articleDiv);
+    }
+    //#region Eventhandling für "in den Einkaufswagen" Button
+    let cartCounter = 0;
+    let priceSum = 0;
+    let cartSpan = document.createElement("span");
+    document.getElementById("cart")?.appendChild(cartSpan);
+    function handleAddToCartClick(_event) {
+        let pressButton = _event.target;
+        let buttonPrice = pressButton.getAttribute("article_price");
+        let priceFloat = parseFloat(buttonPrice);
+        priceSum += priceFloat;
+        console.log("Gesamtwert des Warenkorbs: " + priceSum.toLocaleString("de-DE", { style: "currency", currency: "EUR" }));
+        if (cartCounter == 0) {
+            cartSpan.setAttribute("id", "counter");
+        }
+        cartCounter++;
+        cartSpan.innerText = cartCounter.toString();
+    }
+    //#endregion
+    //#region Eventhandling Artikel über Navbar aus/einblenden
+    Aufgabe07.anchorShowAll.addEventListener("click", showSelectedCategory);
+    Aufgabe07.anchorShowShoes.addEventListener("click", showSelectedCategory);
+    Aufgabe07.anchorShowGear.addEventListener("click", showSelectedCategory);
+    function showSelectedCategory(_event) {
+        if (_event.target.getAttribute("id") == "showall") {
+            document.getElementById("shoes-cat").style.display = "grid";
+            document.getElementById("gear-cat").style.display = "grid";
+            document.getElementById("hgear").style.display = "block";
+            document.getElementById("hshoes").style.display = "block";
+        }
+        else if (_event.target.getAttribute("id") == "showshoes") {
+            document.getElementById("shoes-cat").style.display = "grid";
+            document.getElementById("gear-cat").style.display = "none";
+            document.getElementById("hshoes").style.display = "block";
+            document.getElementById("hgear").style.display = "none";
+        }
+        else if (_event.target.getAttribute("id") == "showgear") {
+            document.getElementById("shoes-cat").style.display = "none";
+            document.getElementById("gear-cat").style.display = "grid";
+            document.getElementById("hshoes").style.display = "none";
+            document.getElementById("hgear").style.display = "block";
+        }
+    }
+    //#endregion 
+})(Aufgabe07 || (Aufgabe07 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Aufgaben/Aufgabe07/script.ts
@@ -0,0 +1,98 @@
+namespace Aufgabe07 {
+
+    for (let i: number = 0; i < articles.length; i++) {
+
+        let articleDiv: HTMLDivElement = document.createElement("div");
+        articleDiv.classList.add("article");
+
+        let articleName: HTMLHeadingElement = document.createElement("h2");
+        articleName.innerText = articles[i].name;
+        articleName.classList.add("article-name");
+        articleDiv.appendChild(articleName);
+
+        let articleIMG: HTMLImageElement = document.createElement("img");
+        articleIMG.setAttribute("alt", articles[i].name);
+        articleIMG.setAttribute("src", articles[i].image);
+        articleIMG.classList.add("article-img");
+        articleDiv.appendChild(articleIMG);
+
+        let articleDesc: HTMLParagraphElement = document.createElement("p");
+        articleDesc.innerText = articles[i].desc;
+        articleDesc.classList.add("article-desc");
+        articleDiv.appendChild(articleDesc);
+
+        let articlePrice: HTMLParagraphElement = document.createElement("p");
+        articlePrice.innerText = articles[i].price.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
+        articlePrice.classList.add("article-price");
+        articleDiv.appendChild(articlePrice);
+
+        let articleButton: HTMLButtonElement = document.createElement("button");
+        articleButton.innerText = "In den Einkaufswagen";
+        articleButton.classList.add("article-button");
+        articleButton.setAttribute("article_price", articles[i].price.toString());
+        articleButton.addEventListener("click", handleAddToCartClick);
+        articleDiv.appendChild(articleButton);
+
+        document.getElementById(articles[i].category + "-cat")?.appendChild(articleDiv);
+
+    }
+
+    //#region Eventhandling für "in den Einkaufswagen" Button
+    let cartCounter: number = 0;
+
+    let priceSum: number = 0;
+
+    let cartSpan: HTMLSpanElement = document.createElement("span");
+    document.getElementById("cart")?.appendChild(cartSpan);
+
+
+    function handleAddToCartClick(_event: Event): void {
+        let pressButton: HTMLButtonElement = <HTMLButtonElement>_event.target;
+
+        let buttonPrice: string = <string>pressButton.getAttribute("article_price");
+
+        let priceFloat: number = parseFloat(buttonPrice);
+
+        priceSum += priceFloat;
+        console.log("Gesamtwert des Warenkorbs: " + priceSum.toLocaleString("de-DE", { style: "currency", currency: "EUR" }));
+
+        if (cartCounter == 0) {
+            cartSpan.setAttribute("id", "counter");
+        }
+        cartCounter++;
+        cartSpan.innerText = cartCounter.toString();
+
+    }
+    //#endregion
+
+    //#region Eventhandling Artikel über Navbar aus/einblenden
+    anchorShowAll.addEventListener("click", showSelectedCategory);
+    anchorShowShoes.addEventListener("click", showSelectedCategory);
+    anchorShowGear.addEventListener("click", showSelectedCategory);
+
+    function showSelectedCategory(_event: Event): void {
+        if ((<HTMLDivElement>_event.target).getAttribute("id") == "showall") {
+            (<HTMLDivElement>document.getElementById("shoes-cat")).style.display = "grid";
+            (<HTMLDivElement>document.getElementById("gear-cat")).style.display = "grid";
+            (<HTMLHeadingElement>document.getElementById("hgear")).style.display = "block";
+            (<HTMLHeadingElement>document.getElementById("hshoes")).style.display = "block";
+
+        }
+        else if ((<HTMLDivElement>_event.target).getAttribute("id") == "showshoes") {
+            (<HTMLDivElement>document.getElementById("shoes-cat")).style.display = "grid";
+            (<HTMLDivElement>document.getElementById("gear-cat")).style.display = "none";
+            (<HTMLHeadingElement>document.getElementById("hshoes")).style.display = "block";
+            (<HTMLHeadingElement>document.getElementById("hgear")).style.display = "none";
+
+        }
+        else if ((<HTMLDivElement>_event.target).getAttribute("id") == "showgear") {
+            (<HTMLDivElement>document.getElementById("shoes-cat")).style.display = "none";
+            (<HTMLDivElement>document.getElementById("gear-cat")).style.display = "grid";
+            (<HTMLHeadingElement>document.getElementById("hshoes")).style.display = "none";
+            (<HTMLHeadingElement>document.getElementById("hgear")).style.display = "block";
+
+        }
+    }
+    //#endregion 
+
+}
